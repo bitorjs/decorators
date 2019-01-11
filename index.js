154cce04fc9ae35d3e3c1d2d5ee907bd88b5fc1e
huangzj <huangzj@huangzj.mzftech.cn>
2019-01-11 12:46:06 +0800

    add service

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('reflect-metadata');
-const metakeys = ["namespace", "Get", "Post", "Delete", "Put", "Head"];
+const methods = ["Get", "Post", "Delete", "Put", "Head"];
+const controller = ['Namespace', 'Controller'];
+const service = ['Service'];
+const metakeys = ["Controller", "Get", "Post", "Delete", "Put", "Head"];
 
-module.exports["methods"] = metakeys.slice(1).map(item => item.toLowerCase());
+module.exports["methods"] = methods.map(item => item.toLowerCase());
 
 metakeys.map(key => {
   module.exports[key] = (path) => (target, name, descriptor) => {
@@ -13,8 +16,17 @@ metakeys.map(key => {
   }
 })
 
+module.exports['Service'] = (service) => (target, name, descriptor) => {
+  Reflect.defineMetadata('Service', service, target, name);
+}
+
+module.exports['getServiceName'] = (classname) => {
+  return Reflect.getMetadata('Service', classname);
+}
+
+
 module.exports['iterator'] = function (classname, callback) {
-  const prefix = Reflect.getMetadata('namespace', classname) || '';
+  const prefix = Reflect.getMetadata('Controller', classname) || '';
   const ownPropertyNames = Object.getOwnPropertyNames(classname['prototype']);
   ownPropertyNames.forEach(propertyName => {
     metakeys.reduce((ret, cur) => {
